a03cf1918aab10302fde2e29d9067bd21450b44f
Hack Reactor Student <student@hackreactor.com>
2018-05-15 11:36:03 -0700

    Add tests for errors thrown and POST structure

--- server/basic-server.js
+++ server/basic-server.js
@@ -9,15 +9,4 @@ var server = http.createServer(handler.requestHandler);
 // console.log('Listening on http://' + ip + ':' + port);
 server.listen(port, ip);
 
-// To start this server, run:
-//
-//   node basic-server.js
-//
-// on the command line.
-//
-// To connect to the server, load http://127.0.0.1:3000 in your web
-// browser.
-//
-// server.listen() will continue running as long as there is the
-// possibility of serving more requests. To stop your server, hit
-// Ctrl-C on the command line.
+module.exports.server = server;
--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -2,6 +2,7 @@ var request = require('request');
 var expect = require('chai').expect;
 
 describe('server', function() {
+
   it('should respond to GET requests for /classes/messages with a 200 status code', function(done) {
     request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
       expect(response.statusCode).to.equal(200);
@@ -47,6 +48,20 @@ describe('server', function() {
     });
   });
 
+  it('should send parsable stringified JSON', function(done) {
+    var requestParams = {method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/messages',
+      json: {
+        username: 'Jono',
+        text: 'Do my bidding!'}
+    };
+
+    request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+      expect(JSON.parse.bind(this, body)).to.not.throw();
+      done();
+    });
+  });
+
   it('should respond with messages that were previously posted', function(done) {
     var requestParams = {method: 'POST',
       uri: 'http://127.0.0.1:3000/classes/messages',
@@ -73,5 +88,11 @@ describe('server', function() {
     });
   });
 
+  it('Should throw an error when asked for a nonexistent endpoint', function(done) {
+    request('http://127.0.0.1:3000/arglebargle', function(error, response, body) {
+      expect(request).to.throw(error);
+      done();
+    });
+  });
 
 });
